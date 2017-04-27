52607a40c783adb81b6ff3d03191752cb7da0048
Pedro Medeiros <pedrosnk@gmail.com>
2017-04-27 17:49:46 -0300

    Frame tests to handle connect and send

---
+++ test/exstomp/frame_test.exs
@@ -0,0 +1,18 @@
+defmodule Exstomp.FrameTest do
+  use ExUnit.Case
+  alias Exstomp.Frame
+
+  test "build a connect frame" do
+    expected = "CONNECT\naccept-version:1.0,1.1\nlogin:\npasscode:\n" <>
+      "heart-beat:10000,7500\nhost:/\n\n\0"
+    assert Frame.build_connect_frame == expected
+  end
+
+  describe "send frame" do
+    test "render text message" do
+      expected = "SEND\ndestination:/queue/foo\ncontent-type:text/plain\n" <>
+        "content-length:7\n\nfoobar\n\0"
+      assert Frame.build_send_frame("/queue/foo", "foobar") == expected
+    end
+  end
+end
--- test/exstomp_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExstompTest do
-  use ExUnit.Case
-  doctest Exstomp
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
